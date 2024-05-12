3f441b57ade3177a3bf510a58fbf1722abdee341
PAndresMoreno <andresm170112@hotmail.com>
2024-05-12 00:38:24 -0500

    Bucles. Ejercicios 13 - 18

--- C6 Bucles/ejercicios/15.js
+++ C6 Bucles/ejercicios/15.js
@@ -6,6 +6,30 @@ function obtenerDiaSemana(numero) {
   // Si el número no corresponde a un día de la semana, retorna
   // el string "No es un dia de la semana"
   // Tu código:
+
+  switch (numero) {
+    case 1:
+      return "Lunes";
+    case 2:
+      return "Martes";
+    case 3:
+      return "Miercoles";
+    case 4:
+      return "Jueves";
+    case 5:
+      return "Viernes";
+    case 6:
+      return "Sabado";
+    case 7:
+      return "Domingo";
+    default:
+      return "No es un dia de la semana";
+  }
+  
+  // let diasDeLaSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+  // if (numero >= 1 && numero <= 7) {
+  //   return diasDeLaSemana [numero -1];
+  // } else return "No es un dia de la semana";
 }
 
 module.exports = obtenerDiaSemana;
--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,13 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let producto = a;
+   for (let i = a + 1; i <= b; i++) {
+      producto = producto * i;
+      if (producto < 0) {
+         producto = producto * -1;
+      }
+   } return producto;
 }
 
 module.exports = productoEntreNúmeros;
