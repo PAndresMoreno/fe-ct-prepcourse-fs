edd330e8876901532700bf126afdab6687ee22c0
PAndresMoreno <andresm170112@hotmail.com>
2024-05-13 21:11:12 -0500

    Arrays. Ejercicios 1 - 7

--- C7 Arrays/ejercicios/06.js
+++ C7 Arrays/ejercicios/06.js
@@ -1,6 +1,14 @@
 function invertirArray(array) {
   // Invierte el arreglo array recibido por argumento.
   // Tu código:
+
+  // array.reverse();
+  // return array;
+  
+  let nuevoArreglo = []
+  for (let i = 0; i < array.length; i++) {
+    nuevoArreglo.unshift(array[i]);
+  } return nuevoArreglo;
 }
 
 module.exports = invertirArray;
--- C7 Arrays/ejercicios/07.js
+++ C7 Arrays/ejercicios/07.js
@@ -2,6 +2,19 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+
+  // array.sort();
+  // return array;
+
+  let elemento = null;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      elemento = array.splice(i, 1);
+      array.push(elemento[0]);
+      i = -1;
+    }
+  } return array;
 }
 
 module.exports = ordenarArray;
