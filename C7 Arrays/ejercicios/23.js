d3a10c2827710c86dc691a60a1f6486e9284d8e9
PAndresMoreno <andresm170112@hotmail.com>
2024-05-23 09:28:54 -0500

    Arrays. Ejercicio 23

--- C7 Arrays/ejercicios/23.js
+++ C7 Arrays/ejercicios/23.js
@@ -3,6 +3,15 @@ function breakStatement(num) {
   // Guardar cada nuevo valor en un arreglo y retórnarlo.
   // Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y retornar el string: "Se interrumpió la ejecución".
   // Tu código:
+  let numerosAumentados = [];
+  let i = 0;
+  
+  while (numerosAumentados.length < 10) {
+    num += 2;
+    numerosAumentados.push(num);
+    if (num === i) return "Se interrumpió la ejecución"
+    i++;
+  } return numerosAumentados;
 }
 
 module.exports = breakStatement;
