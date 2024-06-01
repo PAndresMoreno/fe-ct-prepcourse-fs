23fa73de32803dc92db4dacd21d10f081feffe44
PAndresMoreno <andresm170112@hotmail.com>
2024-06-01 17:20:49 -0500

    Objects + Callbacks / Objects. Ejercicios 11 - 22

--- C8 Objects + Callbacks/objects/ejercicios/20.js
+++ C8 Objects + Callbacks/objects/ejercicios/20.js
@@ -4,6 +4,10 @@ function sumarLikesDeUsuario(objetoUsuario) {
   // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
   // Debes sumar los likes de todos los post y retornar el resultado.
   // Tu código:
+  let likes = 0;
+  for (let i = 0; i < objetoUsuario.posts.length; i++) {
+    likes = likes + objetoUsuario.posts[i].likes;
+  } return likes;
 }
 
 module.exports = sumarLikesDeUsuario;
--- C8 Objects + Callbacks/objects/ejercicios/22.js
+++ C8 Objects + Callbacks/objects/ejercicios/22.js
@@ -5,6 +5,10 @@ function esAnagrama(str1, str2) {
    // IMPORTANTE: Un anagrama es una palabra que se forma
    // con las mismas letras que otra, pero en orden diferente.
    // Tu código:
+   let word1 = str1.toLowerCase().split("").sort().join("");
+   let word2 = str2.toLowerCase().split("").sort().join("");
+
+   return word1 === word2 ? true : false;
 }
 
 module.exports = esAnagrama;
