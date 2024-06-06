c1312fe0b1664bd0ce206ec2cc6c4105b86cbfab
PAndresMoreno <andresm170112@hotmail.com>
2024-06-05 22:01:29 -0500

    Objects + Callbacks / Callbacks. Ejercicios 1 - 8

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,14 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   let palabrasConA = [];
+   for (let i = 0; i < arrayOfStrings.length; i++) {
+      let separarPalabra = arrayOfStrings[i].toLowerCase().split("");
+      if (separarPalabra[0] === "a") {
+         palabrasConA.push(separarPalabra.join(""));
+      }
+   } return palabrasConA;
+
 }
 
 module.exports = filter;
--- C8 Objects + Callbacks/callbacks/ejercicios/08.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/08.js
@@ -3,6 +3,10 @@ const buscarElemento = (array, callback) => {
   // Si el elemento no se encuentra, devuelve el mensje "No se encontró el elemento".
   // La función de callback es la encargada de evaluar si el elemento fue encontrado.
   // Tu código:
+  let elementoEncontrado = "No se encontró el elemento";
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) elementoEncontrado = array[i];
+  } return elementoEncontrado;
 };
 
 module.exports = buscarElemento;
